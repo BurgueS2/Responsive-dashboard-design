0d39e0d576e87ccfa07192a7db92fdf5a1147e67
Felipe Dias <felipedias.101@outlook.com>
2024-01-09 15:39:36 -0300

    Commit

---
+++ assets/js/ordens.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '85743',
+        paymentsStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber : '97245',
+        paymentsStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '36452',
+        paymentsStatus: 'Paid',
+        status: 'Active'
+    },
+]
